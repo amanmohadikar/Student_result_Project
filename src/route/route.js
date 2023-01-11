aff2dbc540feda273deb3b190199d681b978bf83
--replace-all <--add>
2023-01-11 17:29:44 +0530

    all ok

--- src/route/route.js
+++ src/route/route.js
@@ -1,9 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/test",function(req,res){
-    return res.send({message : "ok"})
-})
+
 
 
 module.exports = router
